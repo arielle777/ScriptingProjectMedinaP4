be6f11276ff2a26a8e8c7803c920f81fbc231059
arielle777 <723653@student.dusd.net>
2024-12-03 10:47:47 -0800

    Fixing awake and starting Update
    Fixed error on awaking

---
+++ ScriptingProjectP4Medina/Assets/Scripts/UpdateAndFixedUpdate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateandFixedUpdate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   void FixedUpdate()
+    {
+        Debug.Log("FixedUpdatre time :" + Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
